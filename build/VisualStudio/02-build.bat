0b319989d55ad7866e155d92dc3d765b8af2462c
Ivan Shynkarenka <chronoxor@gmail.com>
2018-08-08 21:14:49 +0300

    build

--- build/VisualStudio/02-build.bat
+++ build/VisualStudio/02-build.bat
@@ -4,6 +4,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../../../../build/VisualStudio
 
 cd ../..
+nuget restore CSharpServer.sln
 MSBuild CSharpServer.sln /p:Configuration=Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd build/VisualStudio
